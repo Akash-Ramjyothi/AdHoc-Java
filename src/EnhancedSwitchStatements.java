f0d4e236a88c47b79076357b3a7310dc6faec4c5
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2026-09-08 00:50:18 +0530

    Added enhanced switch statements

---
+++ src/EnhancedSwitchStatements.java
@@ -0,0 +1,21 @@
+public class EnhancedSwitchStatements {
+    public static void main(String[] args) {
+        int switchValue = 3;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Actually value: " + switchValue);
+            default:
+                System.out.println("Was not 1, 2, 3, 4 or 5");
+                break;
+        }
+    }
+}
