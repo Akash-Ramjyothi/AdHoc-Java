3ccf05ec4f6be0af2b3322e5822481bbd66f3eee
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2026-09-08 01:45:57 +0530

    Implemented yield in switch

--- src/EnhancedSwitchStatements.java
+++ src/EnhancedSwitchStatements.java
@@ -3,19 +3,36 @@ public class EnhancedSwitchStatements {
         int switchValue = 3;
 
         switch (switchValue) {
-            case 1:
-                System.out.println("Value was 1");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            case 3:
-            case 4:
-            case 5:
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
                 System.out.println("Actually value: " + switchValue);
-            default:
-                System.out.println("Was not 1, 2, 3, 4 or 5");
-                break;
+            }
+            default -> System.out.println("Was not 1, 2, 3, 4 or 5");
+        }
+
+        String month = "APRIL123";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+    }
+
+    public static String getQuarter(String month) {
+        switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> {
+                return "1st";
+            }
+            case "APRIL", "MAY", "JUNE" -> {
+                return "2nd";
+            }
+            case "JULY", "AUGUST", "SEPTEMBER" -> {
+                return "3rd";
+            }
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> {
+                return "4th";
+            }
+            default -> {
+                String badResponse = month+ " is bad";
+                return badResponse;
+            }
         }
     }
 }
