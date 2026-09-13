63d0bec169457ba8dad1f7fba83a4426b993423a
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2026-09-13 23:57:28 +0530

    Formatted Speed converter

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,21 +1,21 @@
 public class SpeedConverter {
-    public static long toMilesPerHour(double kilometersPerHour){
-        if(kilometersPerHour<0){
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
             return -1;
         }
 
-        long result = Math.round( (kilometersPerHour / 1.609));
+        long result = Math.round((kilometersPerHour / 1.609));
 
         return result;
     }
 
-    public static void printConversion(double kilometersPerHour){
+    public static void printConversion(double kilometersPerHour) {
         long miles = toMilesPerHour(kilometersPerHour);
-        if(miles==-1){
+        if (miles == -1) {
             System.out.print("Invalid Value");
             return;
         }
-        System.out.print(kilometersPerHour+" km/h = "+miles+" mi/h");
+        System.out.print(kilometersPerHour + " km/h = " + miles + " mi/h");
     }
 
 }
