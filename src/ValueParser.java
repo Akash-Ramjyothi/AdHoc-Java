4a34c54bef88151006f936ed9ddd0eaf93a30f55
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2026-09-14 20:23:22 +0530

    Used Scanner for inputs

--- src/ValueParser.java
+++ src/ValueParser.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class ValueParser {
     public static void main(String[] args) {
         int currentYear = 2022;
 
-        System.out.println(getInputFromConsole(currentYear));
-        System.out.println(getInputFromScanner(currentYear));
+        try {
+            System.out.println(getInputFromConsole(currentYear));
+        } catch (NullPointerException e) {
+            System.out.println(getInputFromScanner(currentYear));
+        }
     }
 
     public static String getInputFromConsole(int currentYear) {
@@ -17,6 +22,19 @@ public class ValueParser {
     }
 
     public static String getInputFromScanner(int currentYear) {
-        return "";
+        Scanner scanner = new Scanner(System.in);
+
+//        String name = System.console().readLine("Enter name: ");
+        System.out.println("Enter name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hi " + name + ", Thanks for taking the course.");
+
+//        String dateOfBirth = System.console().readLine("Enter birth year: ");
+        System.out.println("Enter birth year: ");
+        String dateOfBirth = scanner.nextLine();
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        return "You are " + age + " years old";
     }
 }
